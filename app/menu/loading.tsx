f4c82d5a37de9b8906953a476bcf932953c84bc6
Harold Tan <htan96@icloud.com>
2026-02-14 17:31:12 -0800

    Fix menu loading export

---
+++ app/menu/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <p className="text-lg font-medium">Loading menu...</p>
+    </div>
+  )
+}
