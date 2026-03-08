a4912c39c0cb9ea41a34536a55e401339019e7a0
Harold Tan <htan96@icloud.com>
2026-03-08 11:09:12 -0700

    Fuel redemption balance system

---
+++ sitemap.ts
@@ -0,0 +1,31 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://sunshinesmoothiesvallejo.com",
+      lastModified: new Date(),
+      priority: 1,
+    },
+    {
+      url: "https://sunshinesmoothiesvallejo.com/menu",
+      lastModified: new Date(),
+      priority: 0.9,
+    },
+    {
+      url: "https://sunshinesmoothiesvallejo.com/location",
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
+      url: "https://sunshinesmoothiesvallejo.com/community",
+      lastModified: new Date(),
+      priority: 0.7,
+    },
+    {
+      url: "https://sunshinesmoothiesvallejo.com/about",
+      lastModified: new Date(),
+      priority: 0.7,
+    },
+  ];
+}
