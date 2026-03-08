2b46a0097e12dc54decef5baf67b8f3e2ee16f0e
Harold Tan <htan96@icloud.com>
2026-03-08 12:22:41 -0700

    update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://sunshinesmoothiesvallejo.com",
+      url: "https://sunshinesmoothiesvallejo.com/",
       lastModified: new Date(),
       priority: 1,
     },
